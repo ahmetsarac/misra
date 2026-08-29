a4f54d4da7b0d4e32b8d34178c6a870ea651deb1
Ahmet Saraç <ahmet.src.99@gmail.com>
2026-08-30 02:03:32 +0300

    adding voice blocks between texts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
         applicationId = "com.misra"
         minSdk = 29
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "0.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
